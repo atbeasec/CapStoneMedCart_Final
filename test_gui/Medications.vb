a53940089f0d1a70826441d07c0e86af5fc14408
atbeasec <43078094+atbeasec@users.noreply.github.com>
2021-02-03 17:27:25 -0500

    Updated Medication
    Updated medication calls to use the select and insert methods that are in the database create module

--- test_gui/Medications.vb
+++ test_gui/Medications.vb
@@ -41,4 +41,109 @@
     '/*  WHO                     WHEN        WHAT						*/
     '/*  Alexander Beasecker    1/25/2021   Initial creation            */
     '/*******************************************************************/
+
+
+    '/*********************************************************************/
+    '/*                   SUBROUTINE NAME:InsertMedication                */
+    '/*********************************************************************/
+    '/*                   WRITTEN BY:  	Alexander Beasecker			      */
+    '/*		         DATE CREATED: 	   02/01/21							  */
+    '/*********************************************************************/
+    '/*  SUBROUTINE PURPOSE: The purpose of this subroutine is to insert
+    '/*  a medication record into the medication database. this module takes
+    '/*  in the generic name, RXCUI, dosage, narcotic flag, brand name and 
+    '/* type. it will create a place holder for the barcode. it will
+    '/* then run the sql to insert.
+    '/*********************************************************************/
+    '/*  CALLED BY:   	      									          
+    '/*  (None)								           					  
+    '/*********************************************************************/
+    '/*  CALLS:														    	
+    '/*  SQLiteCommand()
+    '/*  SQLiteConnection()
+    '/*  DBConn.Open()
+    '/*  DBConn.Close()
+    '/*  DBCmd.ExecuteNonQuery()
+    '/*********************************************************************/
+    '/*  PARAMETER LIST (In Parameter Order): StrGenericName, intRXCUI, intDosage
+    '/*   strNarcoticFlag, strBrandName, strType
+    '/*********************************************************************/
+    '/* SAMPLE INVOCATION:								                   
+    '/*	InsertMedication("Generic","234443","100","1","Good Brand","Pill")								                           
+    '/*********************************************************************/
+    '/*  LOCAL VARIABLE LIST (Alphabetically):	
+    '/*  Strdatacommand as string
+    '/*  StrPlaceHolderBarCode as string
+    '/*
+    '/*********************************************************************/
+    '/* MODIFICATION HISTORY:						                      */
+    '/*											                          */
+    '/*  WHO                   WHEN     WHAT							  */
+    '/*  ---                   ----     ----------------------------------*/
+    '/*  Alexander Beasecker  02/01/2021  Initial creation of the code     */
+    '/*********************************************************************/
+    Public Sub InsertMedication(ByRef StrGenericName As String, ByRef intRXCUI As Integer, ByRef intDosage As Integer,
+                                ByRef strNarcoticFlag As String, ByRef strBrandName As String, ByRef strType As String)
+
+        'create SQLite command string and barcode placeholder
+        Dim StrPlaceHolderBarCode As String = "000000000"
+
+        'set up SQL objects
+
+        'set insert statement into sqlite command object
+        Strdatacommand = "INSERT INTO AdHocOrder(Drug_Name, RXCUI_ID, Dosage, NarcoticControlled_Flag, Barcode, Brand_Name, Type)
+                                VALUES('" & StrGenericName & "','" & intRXCUI & "','" & intDosage & "','" & strNarcoticFlag &
+                                "','" & StrPlaceHolderBarCode & "','" & strBrandName & "','" & strType & "')"
+
+        'open connection to datebase, run insert and close
+        CreateDatabase.ExecuteSelectQuery(Strdatacommand)
+
+    End Sub
+
+    '/*********************************************************************/
+    '/*                   SUBROUTINE NAME:UpdateMedicationBarcode         */
+    '/*********************************************************************/
+    '/*                   WRITTEN BY:  	Alexander Beasecker			      */
+    '/*		         DATE CREATED: 	   02/01/21							  */
+    '/*********************************************************************/
+    '/*  SUBROUTINE PURPOSE: The purpose of this subroutine is to update
+    '/*  the barcode attached to a specific medication record. it takes in
+    '/*  the barcode and the RXCUI number for the medication and it will
+    '/*  update that medications record with the new barcode
+    '/*
+    '/*********************************************************************/
+    '/*  CALLED BY:   	      									          
+    '/*  (None)								           					  
+    '/*********************************************************************/
+    '/*  CALLS:														    	
+    '/*  SQLiteCommand()
+    '/*  SQLiteConnection()
+    '/*  DBConn.Open()
+    '/*  DBConn.Close()
+    '/*  DBCmd.ExecuteNonQuery()
+    '/*********************************************************************/
+    '/*  PARAMETER LIST (In Parameter Order): intRXCUI, strBarcode					   		   
+    '/*********************************************************************/
+    '/* SAMPLE INVOCATION:								                   
+    '/*	 UpdateMedicationBarcode("443222","123221233")										                           
+    '/*********************************************************************/
+    '/*  LOCAL VARIABLE LIST (Alphabetically):	
+    '/*	Strdatacommand as string
+    '/*********************************************************************/
+    '/* MODIFICATION HISTORY:						                      */
+    '/*											                          */
+    '/*  WHO                   WHEN     WHAT							  */
+    '/*  ---                   ----     ----------------------------------*/
+    '/*  Alexander Beasecker  02/01/2021  Initial creation of the code    */
+    '/*********************************************************************/
+    Public Sub UpdateMedicationBarcode(ByRef intRXCUI As Integer, ByRef strBarcode As String)
+
+        'create string for SQLite command and create placeHolder string for barcode
+        Dim Strdatacommand As String
+
+        'create update statement for setting the barcode
+        Strdatacommand = "UPDATE Medication SET Barcode ='" & strBarcode & "' WHERE RXCUI = '" & intRXCUI & "';"
+        CreateDatabase.ExecuteInsertQuery(Strdatacommand)
+
+    End Sub
 End Module
