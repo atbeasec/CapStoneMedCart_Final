fb9bc199ae403d6826ba1008bab6bcb509cf2bcf
atbeasec <43078094+atbeasec@users.noreply.github.com>
2021-02-05 12:57:48 -0500

    Drawer Medication  function
    Created a function that will take in a drawer ID and return a dataset of all medications that are currently in that drawer. too be used in the inventory maintinance screen of the GUI

--- test_gui/Inventory.vb
+++ test_gui/Inventory.vb
@@ -51,10 +51,50 @@
 
 
 
+    '/*********************************************************************/
+    '/*                   Function NAME:GetDrawerDrugs                    */
+    '/*********************************************************************/
+    '/*                   WRITTEN BY:  	Alexander Beasecker			      */
+    '/*		         DATE CREATED: 	   02/01/21							  */
+    '/*********************************************************************/
+    '/*  SUBROUTINE PURPOSE: The purpose of this function is to 
+    '/* return a dataset holding all the current medications that are 
+    '/* assigned to a specific drawer
+    '/*********************************************************************/
+    '/*  CALLED BY:   	      									          
+    '/*  (None)								           					  
+    '/*********************************************************************/
+    '/*  CALLS:														    	
+    '/*  CreateDatabase.ExecuteSelectQuery()
+    '/*********************************************************************/
+    '/*  PARAMETER LIST (In Parameter Order): intDrawerID				   		   
+    '/*********************************************************************/
+    '/* SAMPLE INVOCATION:								                   
+    '/*	 dataset = GetDrawerDrugs("1")										                           
+    '/*********************************************************************/
+    '/*  LOCAL VARIABLE LIST (Alphabetically):	
+    '/*	Strdatacommand as string
+    '/* dsDataset as dataset
+    '/*********************************************************************/
+    '/* MODIFICATION HISTORY:						                      */
+    '/*											                          */
+    '/*  WHO                   WHEN     WHAT							  */
+    '/*  ---                   ----     ----------------------------------*/
+    '/*  Alexander Beasecker  02/05/2021  Initial creation of the code    */
+    '/*********************************************************************/
+    Public Function GetDrawerDrugs(ByRef intDrawerID As Integer)
+        'create dataset to hold selected values
+        Dim dsDataset As New DataSet
+        'create select to send to select function
+        Dim Strdatacommand As String = "SELECT Drug_Name, Dosage, Divider_Bin FROM DrawerMedication " &
+            "INNER JOIN Medication N Medication.Medication_ID = DrawerMedication.Medication_TUID " &
+            "HERE DrawerMedication.Drawers_TUID =" & intDrawerID
 
-    Public Sub DisplayDrawerDrugs(ByRef intDrawerID As Integer)
+        'set dataset = to returned dataset from select function from createdatabase file
+        dsDataset = CreateDatabase.ExecuteSelectQuery(Strdatacommand)
 
-
-    End Sub
+        'return dataset that holds drawer medications
+        Return dsDataset
+    End Function
 
 End Module
