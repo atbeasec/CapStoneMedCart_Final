840a41dd04e9e1ac22732552fa333a6f1e24268e
Breanna_Howey <42812674+brhowey@users.noreply.github.com>
2021-02-16 19:58:10 -0500

    Updated the UI for the Add/Delete Rooms and Beds form, allowed for adding rooms and beds, and populated the rooms listbox. Edits still need to be made.

--- test_gui/ConfigureRoomsBeds.vb
+++ test_gui/ConfigureRoomsBeds.vb
@@ -1,12 +1,32 @@
 ﻿Module ConfigureRoomsBeds
+
     Dim strSQLCmd As String
+
     Public Sub AddRoomsBeds(ByVal strRoom As String, ByVal strBed As String, ByVal intActiveFlag As Integer)
         If strRoom <> Nothing Or strBed <> Nothing Then
 
-            '   strSQLCmd = "INSERT INTO Rooms(Room_ID,Bed_Name,User_TUID,Amount,DrawerMedication_TUID,DateTime) " &
-            '        "VALUES('" & intMedicationID & "', '" & intPatientID & "', '" & intUserID & "', '" & intAmount & "', '" & intMedicationDrawerID & "', '" & dtmAdhocTime & "')"
+            strSQLCmd = "INSERT INTO Rooms(Room_ID,Bed_Name,Active_Flag) " &
+                    "VALUES('" & strRoom & "', '" & strBed & "', '" & intActiveFlag & "')"
+
+            ExecuteInsertQuery(strSQLCmd)
+
+            MessageBox.Show("The room and bed has been inserted")
+
         Else
             MessageBox.Show("Please make sure a room and a bed are entered before adding")
         End If
+
+        ShowRooms()
+    End Sub
+
+    Public Sub ShowRooms()
+        Dim dsValues As DataSet = New DataSet
+        strSQLCmd = "SELECT DISTINCT Room_ID FROM Rooms"
+
+        dsValues = ExecuteSelectQuery(strSQLCmd)
+
+        For Each value As DataRow In dsValues.Tables(0).Rows
+            frmConfigureRooms.lstRooms.Items.Add(value(0))
+        Next
     End Sub
 End Module
--- test_gui/frmConfigureRooms.vb
+++ test_gui/frmConfigureRooms.vb
@@ -1,13 +1,16 @@
 ﻿Public Class frmConfigureRooms
     Private Sub frmConfigureRooms_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        ShowRooms()
     End Sub
 
-    Private Sub btnSave_Click(sender As Object, e As EventArgs) 
+    Private Sub btnSave_Click(sender As Object, e As EventArgs)
 
     End Sub
 
     Private Sub btnAddBed_Click(sender As Object, e As EventArgs) Handles btnAddBed.Click
+        AddRoomsBeds(txtRoom.Text, txtBed.Text, 1)
+        txtRoom.Clear()
+        txtBed.Clear()
 
     End Sub
 End Class
