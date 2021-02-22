de1befc9542bcd335b475b009a8256dd6b9fd34d
atbeasec <43078094+atbeasec@users.noreply.github.com>
2021-02-22 13:38:01 -0500

    Update Discrepancies.vb

--- test_gui/Discrepancies.vb
+++ test_gui/Discrepancies.vb
@@ -1,4 +1,6 @@
-﻿Module Discrepancies
+﻿Imports System.Text
+
+Module Discrepancies
     '/*******************************************************************/
     '/*                   FILE NAME: Discrepancies.vb                   */
     '/*******************************************************************/
@@ -58,4 +60,83 @@
     '/*  WHO                     WHEN        WHAT						*/
     '/*  Alexander Beasecker    1/25/2021   Initial creation            */
     '/*******************************************************************/
+
+
+
+
+    '/*********************************************************************/
+    '/*                   FUNCTION NAME: CheckSystemCountVSActualCount    */         
+    '/*********************************************************************/
+    '/*                   WRITTEN BY:     		         */   
+    '/*		         DATE CREATED: 		   */                             
+    '/*********************************************************************/
+    '/*  FUNCTION PURPOSE:								   */             
+    '/*											   */                     
+    '/*                                                                   */
+    '/*********************************************************************/
+    '/*  CALLED BY:   	      						         */           
+    '/*                                         				   */         
+    '/*********************************************************************/
+    '/*  CALLS:										   */                 
+    '/*             (NONE)								   */             
+    '/*********************************************************************/
+    '/*  PARAMETER LIST (In Parameter Order):					   */         
+    '/*											   */                     
+    '/*                                                                     
+    '/*********************************************************************/
+    '/*  RETURNS:								         */                   
+    '/*            (NOTHING)								   */             
+    '/*********************************************************************/
+    '/* SAMPLE INVOCATION:								   */             
+    '/*											   */                     
+    '/*                                                                     
+    '/*********************************************************************/
+    '/*  LOCAL VARIABLE LIST (Alphabetically without hungry notation):    */
+    '/*											   */                     
+    '/*                                                                     
+    '/*********************************************************************/
+    '/* MODIFICATION HISTORY:						         */               
+    '/*											   */                     
+    '/*  WHO   WHEN     WHAT								   */             
+    '/*  ---   ----     ------------------------------------------------- */
+    '/*  AB    2/22/2021 Initial creation
+    '/*********************************************************************/
+
+    Public Function CheckSystemCountVSActualCount(ByRef intSystemCount As Integer, ByRef intActualCount As Integer)
+        Dim blnCountsEqual As Boolean = False
+
+        If intSystemCount = intActualCount Then
+            blnCountsEqual = True
+        Else
+            blnCountsEqual = False
+        End If
+
+        Return blnCountsEqual
+    End Function
+
+
+    Private Sub CreateDiscrepancy(ByRef intDrawerNumber As Integer, ByRef intBinNumber As Integer, ByRef strMedicationName As String,
+                                  ByRef intExpectedCount As Integer, ByRef intActualCount As Integer, ByRef intPrimaryUserID As Integer,
+                                  ByRef intApprovingUserID As Integer)
+        Dim intDrawerTUID As Integer
+        Dim intMedicationTUID As Integer
+
+        If CheckSystemCountVSActualCount(intExpectedCount, intActualCount).Equals(False) Then
+
+            Dim dtmAdhocTime As String = DateTime.Now.ToString("MM/dd/yyyy HH:mm:ss")
+            InsertDiscrepancy(intDrawerTUID, intMedicationTUID, intExpectedCount, intActualCount, intPrimaryUserID, intApprovingUserID, dtmAdhocTime)
+        Else
+            MessageBox.Show("No Discrepancy recorded, counts are equal")
+        End If
+    End Sub
+
+
+    Private Sub InsertDiscrepancy(ByRef intDrawerTUID As Integer, ByRef intBinNumber As Integer, ByRef intMedicationID As Integer,
+                                  ByRef intExpectedCount As Integer, ByRef intActualCount As Integer, ByRef intPrimaryUserID As Integer,
+                                  ByRef intApprovingUserID As Integer, ByRef dtmDateTimeEntered As String)
+
+        Dim strbSQL As StringBuilder = New StringBuilder
+        strbSQL.Append("INSERT INTO Discrepancies(Drawer_TUID, Medication_TUID, Expected_Count, Actual_Count, Primary_User_TUID, Approving_User_TUID, DateTime_Entered) ")
+        strbSQL.Append("VALUES(")
+    End Sub
 End Module
