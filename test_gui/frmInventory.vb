da87e0601092d7abfbbd2407e1feb7a811d67fe1
cmkrygie <cmkrygie@svsu.edu>
2021-02-26 12:17:31 -0500

    Moved the UI elements, made the search work by clicking enter and by clicking the search icon button. moved the icon to search to be after the textbox to be more intuitive

--- test_gui/frmInventory.vb
+++ test_gui/frmInventory.vb
@@ -10,6 +10,8 @@ Public Class frmInventory
         txtSearch.Text = txtSearch.Tag
         txtSearch.ForeColor = Color.Silver
 
+        'Set the focus
+        txtSearch.Select()
 
         DefaultSaveButtonLocation()
         txtQuantity.Text = 1
@@ -183,6 +185,39 @@ Public Class frmInventory
         ' and pass it to the function to find better names
     End Sub
 
+
+    '/*********************************************************************/
+    '/* SubProgram NAME:txtSearch_GotFocus                                */         
+    '/*********************************************************************/
+    '/*                   WRITTEN BY:  Collin Krygier   		          */   
+    '/*		         DATE CREATED: 		 2/26/2021                        */                             
+    '/*********************************************************************/
+    '/*  Subprogram PURPOSE:								              */             
+    '/*	 This sets the behavior of when this control gets focus. We are   */
+    '/*  trying to remove the dummy text when the user enters the field   */
+    '/*********************************************************************/
+    '/*  CALLED BY:   	      						                      */           
+    '/*  None                                                             */
+    '/*********************************************************************/
+    '/*  CALLS:										                      */                 
+    '/*                                    */
+    '/*********************************************************************/
+    '/*  PARAMETER LIST (In Parameter Order):					          */         
+    '/*	 sender- object representing a control                            */
+    '/*  e- eventargs indicating there is an event handle assigned        */
+    '/*********************************************************************/
+    '/* SAMPLE INVOCATION:								                  */             
+    '/*                                       */     
+    '/*********************************************************************/
+    '/*  LOCAL VARIABLE LIST (Alphabetically without hungry notation):    */
+    '/*	 None                                                             */
+    '/*********************************************************************/
+    '/* MODIFICATION HISTORY:						                      */               
+    '/*											                          */                     
+    '/*  WHO   WHEN     WHAT								              */             
+    '/*  ---   ----     ------------------------------------------------  */
+    '/*  Collin Krygier  2/16/2021    Initial creation                    */
+    '/*********************************************************************/
     Private Sub txtSearch_GotFocus(sender As Object, e As EventArgs) Handles txtSearch.GotFocus
 
         ' when the search box gains focus, we will remove the default text only if the user has not typed in it yet
@@ -194,9 +229,40 @@ Public Class frmInventory
 
         End If
 
-
     End Sub
 
+    '/*********************************************************************/
+    '/* SubProgram NAME:txtSearch_GotFocus                                */         
+    '/*********************************************************************/
+    '/*                   WRITTEN BY:  Collin Krygier   		          */   
+    '/*		         DATE CREATED: 		 2/26/2021                        */                             
+    '/*********************************************************************/
+    '/*  Subprogram PURPOSE:								              */             
+    '/*	 This sets the behavior of when this control loses focus. We will */
+    '/* reset the text to the dummy text if it is empty, otherwise keep txt/
+    '/*********************************************************************/
+    '/*  CALLED BY:   	      						                      */           
+    '/*  None                                                             */
+    '/*********************************************************************/
+    '/*  CALLS:										                      */                 
+    '/*                          */
+    '/*********************************************************************/
+    '/*  PARAMETER LIST (In Parameter Order):					          */         
+    '/*	 sender- object representing a control                            */
+    '/*  e- eventargs indicating there is an event handle assigned        */
+    '/*********************************************************************/
+    '/* SAMPLE INVOCATION:								                  */             
+    '/*                                     */     
+    '/*********************************************************************/
+    '/*  LOCAL VARIABLE LIST (Alphabetically without hungry notation):    */
+    '/*	 None                                                             */
+    '/*********************************************************************/
+    '/* MODIFICATION HISTORY:						                      */               
+    '/*											                          */                     
+    '/*  WHO   WHEN     WHAT								              */             
+    '/*  ---   ----     ------------------------------------------------  */
+    '/*  Collin Krygier  2/16/2021    Initial creation                    */
+    '/*********************************************************************/
 
     Private Sub txtSearch_LostFocus(sender As Object, e As EventArgs) Handles txtSearch.LostFocus
 
@@ -207,5 +273,129 @@ Public Class frmInventory
             txtSearch.Text = txtSearch.Tag
             txtSearch.ForeColor = Color.Silver
         End If
+
+    End Sub
+
+
+    '/*********************************************************************/
+    '/* SubProgram NAME:txtSearch_TextChanged                             */         
+    '/*********************************************************************/
+    '/*                   WRITTEN BY:  Collin Krygier   		          */   
+    '/*		         DATE CREATED: 		 2/26/2021                        */                             
+    '/*********************************************************************/
+    '/*  Subprogram PURPOSE:								              */             
+    '/*	 This method ensures that when the user selects the enter key the */
+    '/* the textbox does not go to the nextline, and that it searches.    */
+    '/*********************************************************************/
+    '/*  CALLED BY:   	      						                      */           
+    '/*  None                                                             */
+    '/*********************************************************************/
+    '/*  CALLS:										                      */                 
+    '/*                          */
+    '/*********************************************************************/
+    '/*  PARAMETER LIST (In Parameter Order):					          */         
+    '/*	 sender- object representing a control                            */
+    '/*  e- eventargs indicating there is an event handle assigned        */
+    '/*********************************************************************/
+    '/* SAMPLE INVOCATION:								                  */             
+    '/*                                     */     
+    '/*********************************************************************/
+    '/*  LOCAL VARIABLE LIST (Alphabetically without hungry notation):    */
+    '/*	 None                                                             */
+    '/*********************************************************************/
+    '/* MODIFICATION HISTORY:						                      */               
+    '/*											                          */                     
+    '/*  WHO   WHEN     WHAT								              */             
+    '/*  ---   ----     ------------------------------------------------  */
+    '/*  Collin Krygier  2/16/2021    Initial creation                    */
+    '/*********************************************************************/
+    Private Sub txtSearch_TextChanged(sender As Object, e As EventArgs) Handles txtSearch.TextChanged
+
+        ' detects if there has been another line added to the textbox
+        ' indicating the user has selected the "enter" key
+        If txtSearch.Lines.Length > 1 Then
+
+            ' since we know the user selected enter and we are using a multiline textbox,
+            ' the text input will be equal to whatever the user typed + a CRLF character
+            ' we will replace that character with an empty string as if it was never typed.
+            Dim singleLine = txtSearch.Text.Replace(vbCrLf, "")
+
+            ' reset the textbox to be empty because it currently contains the user types string + CRLF
+            txtSearch.Text = ""
+
+            ' set the textbox to contain the searched word on a single line
+            txtSearch.Text = singleLine
+
+            ' by default VB will move the text cursor position to be at the first character after adding
+            ' a new string to the textbox. This looks weird and seems like a bug to the user when the
+            ' cursor position moves from the last character to the first. We will set to be the last 
+            ' with the code below.
+            txtSearch.Select(txtSearch.Text.Length, 0)
+
+
+
+
+
+            '*********************************
+            'Call method here to do the search
+            '*********************************
+
+
+
+
+
+
+            'set the focus to the next control because it should be populated 
+            cmbMedicationName.Select()
+
+        End If
+
+    End Sub
+
+    '/*********************************************************************/
+    '/* SubProgram NAME:pnlSearchIcon_Click                               */         
+    '/*********************************************************************/
+    '/*                   WRITTEN BY:  Collin Krygier   		          */   
+    '/*		         DATE CREATED: 		 2/26/2021                        */                             
+    '/*********************************************************************/
+    '/*  Subprogram PURPOSE:								              */             
+    '/*	 The user can select the search icon to search as an alternative  */
+    '/*  clicking enter                                                   */
+    '/*********************************************************************/
+    '/*  CALLED BY:   	      						                      */           
+    '/*  None                                                             */
+    '/*********************************************************************/
+    '/*  CALLS:										                      */                 
+    '/*                          */
+    '/*********************************************************************/
+    '/*  PARAMETER LIST (In Parameter Order):					          */         
+    '/*	 sender- object representing a control                            */
+    '/*  e- eventargs indicating there is an event handle assigned        */
+    '/*********************************************************************/
+    '/* SAMPLE INVOCATION:								                  */             
+    '/*                                     */     
+    '/*********************************************************************/
+    '/*  LOCAL VARIABLE LIST (Alphabetically without hungry notation):    */
+    '/*	 None                                                             */
+    '/*********************************************************************/
+    '/* MODIFICATION HISTORY:						                      */               
+    '/*											                          */                     
+    '/*  WHO   WHEN     WHAT								              */             
+    '/*  ---   ----     ------------------------------------------------  */
+    '/*  Collin Krygier  2/16/2021    Initial creation                    */
+    '/*********************************************************************/
+
+    Private Sub pnlSearchIcon_Click(sender As Object, e As EventArgs) Handles pnlSearch.Click
+
+        MessageBox.Show("Searched")
+        '*********************************
+        'Call method here to do the search
+        '*********************************
+
+
+        'set the focus to the next control because it should be populated 
+        cmbMedicationName.Select()
+
+
     End Sub
 End Class
