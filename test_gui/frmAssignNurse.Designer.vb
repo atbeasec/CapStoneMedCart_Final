9bf85e59b1a485aff53d4aa9d2c75d25f0aa5be3
cmkrygie <cmkrygie@svsu.edu>
2021-03-10 12:15:02 -0500

    Made background white on the Assign nurse form

--- test_gui/frmAssignNurse.Designer.vb
+++ test_gui/frmAssignNurse.Designer.vb
@@ -28,6 +28,7 @@ Partial Class frmAssignNurse
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(877, 690)
         Me.Name = "frmAssignNurse"
         Me.Text = "frmAssignNurse"
