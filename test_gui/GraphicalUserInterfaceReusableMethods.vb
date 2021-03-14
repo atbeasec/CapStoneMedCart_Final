858bea1b32b86a969021d1a30ae5ed3b2047c6cb
cmkrygie <cmkrygie@svsu.edu>
2021-03-14 15:31:03 -0400

    added comment block to the new method

--- test_gui/GraphicalUserInterfaceReusableMethods.vb
+++ test_gui/GraphicalUserInterfaceReusableMethods.vb
@@ -972,6 +972,43 @@ Module GraphicalUserInterfaceReusableMethods
 
     End Sub
 
+    '/*********************************************************************/
+    '/*                   Function NAME: TruncateString()                 */         
+    '/*********************************************************************/
+    '/*              WRITTEN BY:  Collin Krygier          		          */   
+    '/*		         DATE CREATED: 		 3/14/2021                        */                             
+    '/*********************************************************************/
+    '/*  Function PURPOSE:								                  */             
+    '/*	 This function simply takes a string and truncates it to a new    */
+    '/*  length if the string is longer than the specified length. If not,*/
+    '/*  it will be left alone and we return the original string passed in*/
+    '/*********************************************************************/
+    '/*  Function Return Value:					                          */         
+    '/*	 A string that is possibly shorter version of what was passed in  */
+    '/*********************************************************************/
+    '/*  CALLED BY:   	      						                      */           
+    '/*  CreatePanel()                                                    */
+    '/*********************************************************************/
+    '/*  CALLS:										                      */                 
+    '/* None                                                              */  
+    '/*********************************************************************/
+    '/*  PARAMETER LIST (In Parameter Order):					          */         
+    '/*	idealLength- integer representing the length we want to truncate at/ 
+    '/*	wordToTruncate- a string that will be truncated if its longer than /
+    '/* the ideal length in characters.                                   */ 
+    '/*********************************************************************/
+    '/* SAMPLE INVOCATION:								                  */             
+    '/*	TruncateString(15, "hello")                                		  */     
+    '/*********************************************************************/
+    '/*  LOCAL VARIABLE LIST (Alphabetically without hungry notation):    */
+    '/*	None                                                              */
+    '/*********************************************************************/
+    '/* MODIFICATION HISTORY:						                      */               
+    '/*											                          */                     
+    '/*  WHO   WHEN     WHAT								              */             
+    '/*  ---   ----     ------------------------------------------------  */
+    '/*  Collin Krygier  3/14/2021    Initial creation                    */
+    '/*********************************************************************/
     Public Function TruncateString(ByVal idealLength As Integer, ByVal wordToTruncate As String) As String
 
         ' checking if the length of the word is longer than the ideal length. This needs to be done because
