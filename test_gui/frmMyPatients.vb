ac778d3ae543c4a7fcc3f9d5e1aec2857033b5ef
ackott <42776213+ackott@users.noreply.github.com>
2021-03-16 11:23:15 -0400

    Added Functionality for Radio Buttons #272

--- test_gui/frmMyPatients.vb
+++ test_gui/frmMyPatients.vb
@@ -1,5 +1,10 @@
 ﻿Public Class frmMyPatients
     Private Sub frmMyPatients_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        rdbShowAll.Checked = True
+        LoadPanel()
+
+    End Sub
+    Private Sub LoadPanel()
         Dim UserID As Integer = 9
         Dim strUserFirst As String = ""
         Dim strUserLast As String = ""
@@ -20,36 +25,61 @@
         dsUser = CreateDatabase.ExecuteSelectQuery("Select User_First_Name, User_Last_Name From User Where User_ID =" & UserID.ToString & " ;")
         For Each row As DataRow In dsUser.Tables(0).Rows
             strUserFirst = row(0)
+
             strUserLast = row(1)
-        Next
-        txtPhysician.Text = (strUserFirst + " " + strUserLast + ", " + UserID.ToString())
+            Next
+            txtPhysician.Text = (strUserFirst + " " + strUserLast + ", " + UserID.ToString())
         If rdbShowAll.Checked = True Then
             dsPatientUser = CreateDatabase.ExecuteSelectQuery("Select * From PatientUser Where User_TUID =" & UserID.ToString & ";")
+            For Each row As DataRow In dsPatientUser.Tables(0).Rows
+                intPatientID = row(0)
+                strVisitDate = row(2)
+                dsPatient = CreateDatabase.ExecuteSelectQuery("Select MRN_Number, Patient_First_Name, Patient_Last_Name, Date_of_Birth, Primary_Physician_ID, Patient.Patient_ID, PatientRoom.Patient_TUID, Room_TUID, Bed_Name FROM Patient LEFT JOIN PatientRoom ON PatientRoom.Patient_TUID = Patient.Patient_ID Where Patient_ID =" & intPatientID.ToString() & ";")
+                For Each Patient As DataRow In dsPatient.Tables(0).Rows
+                    intPatientMRN = Patient(0)
+                    strPatientFirst = Patient(1)
+                    strPatientLast = Patient(2)
+                    StrDOB = Patient(3)
+                    intPhysicianID = Patient(4)
+                    strRoom = Patient(7)
+                    strBed = Patient(8)
+                    'StrDOB = StrDOB.Substring(0, 9)
+                    Debug.WriteLine("")
+                    frmPatientRecords.CreatePanel(flpMyPatientRecords, intPatientMRN.ToString, strPatientFirst, strPatientLast, StrDOB, strRoom, strBed)
+                Next
+
+                Debug.WriteLine("")
+
+            Next
         ElseIf rdbOnlyActive.Checked = True Then
             dsPatientUser = CreateDatabase.ExecuteSelectQuery("Select * From PatientUser Where User_TUID =" & UserID.ToString & " AND Active_Flag = 1 ;")
+            For Each row As DataRow In dsPatientUser.Tables(0).Rows
+                intPatientID = row(0)
+                strVisitDate = row(2)
+                dsPatient = CreateDatabase.ExecuteSelectQuery("Select MRN_Number, Patient_First_Name, Patient_Last_Name, Date_of_Birth, Primary_Physician_ID, Patient.Patient_ID, PatientRoom.Patient_TUID, Room_TUID, Bed_Name FROM Patient LEFT JOIN PatientRoom ON PatientRoom.Patient_TUID = Patient.Patient_ID Where Patient_ID =" & intPatientID.ToString() & ";")
+                For Each Patient As DataRow In dsPatient.Tables(0).Rows
+                    intPatientMRN = Patient(0)
+                    strPatientFirst = Patient(1)
+                    strPatientLast = Patient(2)
+                    StrDOB = Patient(3)
+                    intPhysicianID = Patient(4)
+                    strRoom = Patient(7)
+                    strBed = Patient(8)
+                    'StrDOB = StrDOB.Substring(0, 9)
+                    Debug.WriteLine("")
+                    frmPatientRecords.CreatePanel(flpMyPatientRecords, intPatientMRN.ToString, strPatientFirst, strPatientLast, StrDOB, strRoom, strBed)
+                Next
+
+                Debug.WriteLine("")
+
+            Next
         End If
 
-        For Each row As DataRow In dsPatientUser.Tables(0).Rows
-            intPatientID = row(0)
-            strVisitDate = row(2)
-            intActive_Flag = row(3)
-            dsPatient = CreateDatabase.ExecuteSelectQuery("Select MRN_Number, Patient_First_Name, Patient_Last_Name, Date_of_Birth, Primary_Physician_ID, Patient.Patient_ID, PatientRoom.Patient_TUID, Room_TUID, Bed_Name FROM Patient LEFT JOIN PatientRoom ON PatientRoom.Patient_TUID = Patient.Patient_ID Where Patient_ID =" & intPatientID.ToString() & ";")
-            For Each Patient As DataRow In dsPatient.Tables(0).Rows
-                intPatientMRN = Patient(0)
-                strPatientFirst = Patient(1)
-                strPatientLast = Patient(2)
-                StrDOB = Patient(3)
-                intPhysicianID = Patient(4)
-                strRoom = Patient(7)
-                strBed = Patient(8)
-                'StrDOB = StrDOB.Substring(0, 9)
-                Debug.WriteLine("")
-                frmPatientRecords.CreatePanel(flpMyPatientRecords, intPatientMRN.ToString, strPatientFirst, strPatientLast, StrDOB, strRoom, strBed)
-            Next
 
-            Debug.WriteLine("")
+    End Sub
 
-        Next
-
+    Private Sub rdbOnlyActive_CheckedChanged(sender As Object, e As EventArgs) Handles rdbOnlyActive.CheckedChanged
+        flpMyPatientRecords.Controls.Clear()
+        LoadPanel()
     End Sub
 End Class
