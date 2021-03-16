20180539104d3ee670996c6739e09cd1d2b9398a
ackott <42776213+ackott@users.noreply.github.com>
2021-03-16 11:03:09 -0400

    Added updating panels for My patients #272

--- test_gui/frmMyPatients.vb
+++ test_gui/frmMyPatients.vb
@@ -1,25 +1,54 @@
 ﻿Public Class frmMyPatients
     Private Sub frmMyPatients_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim PhysicianID As Integer = 2
-        Dim strPhysicianFirst As String = ""
-        Dim strPhysicianLast As String = ""
-        Dim dsPatientPhysician As DataSet
-        Dim dsPhysician As DataSet
+        Dim UserID As Integer = 9
+        Dim strUserFirst As String = ""
+        Dim strUserLast As String = ""
+        Dim strVisitDate As String = ""
+        Dim dsPatientUser As DataSet
+        Dim dsUser As DataSet
+        Dim dsPatient As DataSet
+        Dim intPhysicianID As Integer = 0
         Dim intPatientID As Integer = 0
-        dsPhysician = CreateDatabase.ExecuteSelectQuery("Select Physician_First_Name, Physician_Last_Name From Physician Where Physician_ID =" & PhysicianID.ToString & " ;")
-        For Each row As DataRow In dsPhysician.Tables(0).Rows
-            strPhysicianFirst = row(0)
-            strPhysicianLast = row(1)
+        Dim intPatientMRN As Integer = 0
+        Dim strPatientFirst As String = ""
+        Dim strPatientLast As String = ""
+        Dim StrDOB As String = ""
+        Dim strRoom As String = ""
+        Dim strBed As String = ""
+        Dim intActive_Flag As String = ""
+
+        dsUser = CreateDatabase.ExecuteSelectQuery("Select User_First_Name, User_Last_Name From User Where User_ID =" & UserID.ToString & " ;")
+        For Each row As DataRow In dsUser.Tables(0).Rows
+            strUserFirst = row(0)
+            strUserLast = row(1)
         Next
-        txtPhysician.Text = (strPhysicianFirst + " " + strPhysicianLast + ", " + PhysicianID.ToString())
+        txtPhysician.Text = (strUserFirst + " " + strUserLast + ", " + UserID.ToString())
         If rdbShowAll.Checked = True Then
-            dsPatientPhysician = CreateDatabase.ExecuteSelectQuery("Select * From PatientPhysician Where Physician_ID =" & PhysicianID.ToString & ";")
+            dsPatientUser = CreateDatabase.ExecuteSelectQuery("Select * From PatientUser Where User_TUID =" & UserID.ToString & ";")
         ElseIf rdbOnlyActive.Checked = True Then
-            dsPatientPhysician = CreateDatabase.ExecuteSelectQuery("Select * From PatientPhysician Where Physician_ID =" & PhysicianID.ToString & " AND Active_Flag = 1 ;")
+            dsPatientUser = CreateDatabase.ExecuteSelectQuery("Select * From PatientUser Where User_TUID =" & UserID.ToString & " AND Active_Flag = 1 ;")
         End If
 
-        For Each row As DataRow In dsPatientPhysician.Tables(0).Rows
+        For Each row As DataRow In dsPatientUser.Tables(0).Rows
             intPatientID = row(0)
+            strVisitDate = row(2)
+            intActive_Flag = row(3)
+            dsPatient = CreateDatabase.ExecuteSelectQuery("Select MRN_Number, Patient_First_Name, Patient_Last_Name, Date_of_Birth, Primary_Physician_ID, Patient.Patient_ID, PatientRoom.Patient_TUID, Room_TUID, Bed_Name FROM Patient LEFT JOIN PatientRoom ON PatientRoom.Patient_TUID = Patient.Patient_ID Where Patient_ID =" & intPatientID.ToString() & ";")
+            For Each Patient As DataRow In dsPatient.Tables(0).Rows
+                intPatientMRN = Patient(0)
+                strPatientFirst = Patient(1)
+                strPatientLast = Patient(2)
+                StrDOB = Patient(3)
+                intPhysicianID = Patient(4)
+                strRoom = Patient(7)
+                strBed = Patient(8)
+                'StrDOB = StrDOB.Substring(0, 9)
+                Debug.WriteLine("")
+                frmPatientRecords.CreatePanel(flpMyPatientRecords, intPatientMRN.ToString, strPatientFirst, strPatientLast, StrDOB, strRoom, strBed)
+            Next
+
+            Debug.WriteLine("")
+
         Next
 
     End Sub
