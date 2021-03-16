070593e61a0c1cdd5edf269167c92b972a0e73c1
daperron <56853565+daperron@users.noreply.github.com>
2021-03-16 01:22:02 -0400

    Progress Bar loads using Time
    Progress bar moves based on time passed, used as a test to make sure progress bar will work inside its form

--- test_gui/frmProgressBar.Designer.vb
+++ test_gui/frmProgressBar.Designer.vb
@@ -22,7 +22,10 @@ Partial Class frmProgressBar
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.pbrProbgressBar = New System.Windows.Forms.ProgressBar()
+        Me.tmTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.lblMessageUpdate = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'pbrProbgressBar
@@ -32,16 +35,33 @@ Partial Class frmProgressBar
         Me.pbrProbgressBar.Size = New System.Drawing.Size(482, 23)
         Me.pbrProbgressBar.TabIndex = 0
         '
+        'tmTimer
+        '
+        Me.tmTimer.Enabled = True
+        '
+        'lblMessageUpdate
+        '
+        Me.lblMessageUpdate.AutoSize = True
+        Me.lblMessageUpdate.Location = New System.Drawing.Point(92, 43)
+        Me.lblMessageUpdate.Name = "lblMessageUpdate"
+        Me.lblMessageUpdate.Size = New System.Drawing.Size(78, 13)
+        Me.lblMessageUpdate.TabIndex = 1
+        Me.lblMessageUpdate.Text = "Waiting to start"
+        '
         'frmProgressBar
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(506, 150)
+        Me.Controls.Add(Me.lblMessageUpdate)
         Me.Controls.Add(Me.pbrProbgressBar)
         Me.Name = "frmProgressBar"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents pbrProbgressBar As Windows.Forms.ProgressBar
+    Friend WithEvents tmTimer As Timer
+    Friend WithEvents lblMessageUpdate As Label
 End Class
