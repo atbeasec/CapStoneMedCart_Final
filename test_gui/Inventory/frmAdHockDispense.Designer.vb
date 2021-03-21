b46ebd7dcf326401bd22d4edd908a9eb2d816dbc
atbeasec <43078094+atbeasec@users.noreply.github.com>
2021-03-20 23:33:54 -0400

    update

--- test_gui/Inventory/frmAdHockDispense.Designer.vb
+++ test_gui/Inventory/frmAdHockDispense.Designer.vb
@@ -254,6 +254,7 @@ Partial Class frmAdHockDispense
         Me.txtStrength.MaxLength = 9
         Me.txtStrength.Name = "txtStrength"
         Me.txtStrength.ReadOnly = True
+        Me.txtStrength.ShortcutsEnabled = False
         Me.txtStrength.Size = New System.Drawing.Size(293, 25)
         Me.txtStrength.TabIndex = 218
         '
@@ -266,6 +267,7 @@ Partial Class frmAdHockDispense
         Me.txtType.MaxLength = 9
         Me.txtType.Name = "txtType"
         Me.txtType.ReadOnly = True
+        Me.txtType.ShortcutsEnabled = False
         Me.txtType.Size = New System.Drawing.Size(321, 25)
         Me.txtType.TabIndex = 219
         '
@@ -288,6 +290,7 @@ Partial Class frmAdHockDispense
         Me.txtDrawerBin.MaxLength = 9
         Me.txtDrawerBin.Name = "txtDrawerBin"
         Me.txtDrawerBin.ReadOnly = True
+        Me.txtDrawerBin.ShortcutsEnabled = False
         Me.txtDrawerBin.Size = New System.Drawing.Size(293, 25)
         Me.txtDrawerBin.TabIndex = 221
         '
@@ -300,6 +303,7 @@ Partial Class frmAdHockDispense
         Me.txtMRN.MaxLength = 9
         Me.txtMRN.Name = "txtMRN"
         Me.txtMRN.ReadOnly = True
+        Me.txtMRN.ShortcutsEnabled = False
         Me.txtMRN.Size = New System.Drawing.Size(294, 25)
         Me.txtMRN.TabIndex = 222
         '
@@ -312,6 +316,7 @@ Partial Class frmAdHockDispense
         Me.txtDateOfBirth.MaxLength = 9
         Me.txtDateOfBirth.Name = "txtDateOfBirth"
         Me.txtDateOfBirth.ReadOnly = True
+        Me.txtDateOfBirth.ShortcutsEnabled = False
         Me.txtDateOfBirth.Size = New System.Drawing.Size(322, 25)
         Me.txtDateOfBirth.TabIndex = 223
         '
@@ -324,6 +329,7 @@ Partial Class frmAdHockDispense
         Me.txtRoomBed.MaxLength = 9
         Me.txtRoomBed.Name = "txtRoomBed"
         Me.txtRoomBed.ReadOnly = True
+        Me.txtRoomBed.ShortcutsEnabled = False
         Me.txtRoomBed.Size = New System.Drawing.Size(294, 25)
         Me.txtRoomBed.TabIndex = 225
         '
