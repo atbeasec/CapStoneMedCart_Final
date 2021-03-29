2f12edbf7b398b2c6bca81a9232763f90481e773
atbeasec <43078094+atbeasec@users.noreply.github.com>
2021-03-29 08:53:55 -0400

    Update frmAdHockDispense.Designer.vb

--- test_gui/Inventory/frmAdHockDispense.Designer.vb
+++ test_gui/Inventory/frmAdHockDispense.Designer.vb
@@ -93,8 +93,9 @@ Partial Class frmAdHockDispense
         '
         'cmbMedications
         '
+        Me.cmbMedications.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cmbMedications.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
         Me.cmbMedications.BackColor = System.Drawing.Color.White
-        Me.cmbMedications.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cmbMedications.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cmbMedications.FormattingEnabled = True
         Me.cmbMedications.Items.AddRange(New Object() {"Yes", "No"})
@@ -137,8 +138,9 @@ Partial Class frmAdHockDispense
         '
         'cmbPatientName
         '
+        Me.cmbPatientName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend
+        Me.cmbPatientName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.ListItems
         Me.cmbPatientName.BackColor = System.Drawing.Color.White
-        Me.cmbPatientName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
         Me.cmbPatientName.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.cmbPatientName.FormattingEnabled = True
         Me.cmbPatientName.Items.AddRange(New Object() {"Yes", "No"})
